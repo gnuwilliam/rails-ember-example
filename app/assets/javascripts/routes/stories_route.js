d56fa58e908d2b408f2cb7ebbf47a31e02e7e314
William Oliveira <sudowilliam@gmail.com>
2013-12-28 02:14:31 -0200

    specify model

--- app/assets/javascripts/routes/stories_route.js
+++ app/assets/javascripts/routes/stories_route.js
@@ -1,5 +1,7 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 RailsEmberExample.StoriesRoute = Ember.Route.extend({
-
+  model: function() {
+    return this.store.find('story');
+  }
 });

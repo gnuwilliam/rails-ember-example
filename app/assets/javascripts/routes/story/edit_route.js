9d5808c34b98633d5e4cee27257c74c3116dbaea
William Oliveira <sudowilliam@gmail.com>
2013-12-28 03:53:25 -0200

    configure edit route

--- app/assets/javascripts/routes/story/edit_route.js
+++ app/assets/javascripts/routes/story/edit_route.js
@@ -1,5 +1,7 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 RailsEmberExample.StoryEditRoute = Ember.Route.extend({
-
+  model: function () {
+    return this.modelFor('story');
+  }
 });

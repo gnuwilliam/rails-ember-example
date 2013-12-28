0dbc4517624962821399a021e60069521c3b166e
William Oliveira <sudowilliam@gmail.com>
2013-12-28 02:45:59 -0200

    configure nested view for story

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,7 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 RailsEmberExample.Router.map(function() {
-  this.resource('stories');
-  this.resource('story', { path: '/stories/:story_id' });
+  this.resource('stories', function () {
+    this.resource('story', { path: ':story_id' });
+  });
 });
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,4 +2,8 @@ class StoriesController < ApplicationController
   def index
     render json: Story.all
   end
+
+  def show
+    render json: Story.find(params[:id])
+  end
 end

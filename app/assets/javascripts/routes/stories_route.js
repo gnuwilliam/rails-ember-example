8b5db92ed7fdac1f54f2e02b145ed3d42585d88c
William Oliveira <sudowilliam@gmail.com>
2013-12-28 03:02:58 -0200

    return only commited stories

--- app/assets/javascripts/routes/stories_route.js
+++ app/assets/javascripts/routes/stories_route.js
@@ -2,6 +2,10 @@
 
 RailsEmberExample.StoriesRoute = Ember.Route.extend({
   model: function() {
-    return this.store.find('story');
+    this.store.find('story');
+
+    return this.store.filter('story', function (story) {
+      return !story.get('isNew');
+    });
   }
 });

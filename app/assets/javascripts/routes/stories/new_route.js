141e439004950b30adaf9d83c513c19a03a63a83
William Oliveira <sudowilliam@gmail.com>
2013-12-28 03:28:34 -0200

    action to create new story

--- app/assets/javascripts/routes/stories/new_route.js
+++ app/assets/javascripts/routes/stories/new_route.js
@@ -2,6 +2,17 @@
 
 RailsEmberExample.StoriesNewRoute = Ember.Route.extend({
   model: function () {
-    this.store.createRecord('story');
+    return this.store.createRecord('story');
+  },
+
+  actions: {
+    create: function (story) {
+      console.log(story);
+      var route = this;
+
+      story.save().then(function () {
+        route.transitionTo('stories');
+      });
+    }
   }
 });

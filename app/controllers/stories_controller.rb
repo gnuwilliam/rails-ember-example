e4187ad0089ba865909fb8d5295ac2d1344acbe5
William Oliveira <sudowilliam@gmail.com>
2013-12-28 04:01:52 -0200

    implements update method on rails story controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -12,6 +12,10 @@ class StoriesController < ApplicationController
     render json: Story.create(story)
   end
 
+  def update
+    render json: Story.find(params[:id]).tap { |s| s.update_attributes(story) }
+  end
+
   private
   def story
     params[:story].permit(:title, :body)
